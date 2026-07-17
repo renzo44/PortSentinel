9b8ff807a6ba0234f0dab5d27a459a28e764eca9
Renzo44 <renzoantonacci44@gmail.com>
2026-07-17 10:39:08 +0200

    Add banner grabbing support

--- network.py
+++ network.py
@@ -14,13 +14,31 @@ def scan_port(ip, port):
     return resultado == 0
 
 
+def get_banner(ip, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2)
+
+        s.connect((ip, port))
+
+        banner = s.recv(1024).decode(errors="ignore")
+
+        s.close()
+
+        return banner.strip()
+
+    except:
+        return "N/A"
+
 def scan_range(ip, puerto_inicial, puerto_final):
 
-    print("PORT     STATUS   SERVICE")
-    print("-" * 30)
+    print("PORT     STATUS   SERVICE     BANNER")
+    print("-" * 50)
 
     for puerto in range(puerto_inicial, puerto_final + 1):
 
         if scan_port(ip, puerto):
             service = get_service(puerto)
-            print(f"{puerto:<8} OPEN     {service}")
+            banner = get_banner(ip, puerto)
+
+            print(f"{puerto:<8} OPEN     {service:<10} {banner}")
